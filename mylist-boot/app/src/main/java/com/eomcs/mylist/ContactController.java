e11561998cf8bf0e0d4ca66094088de89f96c50d
InfraWhale <gyqjajang@gmail.com>
2021-12-14 01:26:37 +0900

    2021-12-14

--- mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/ContactController.java
@@ -1,42 +1,76 @@
 package com.eomcs.mylist;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController //클라이언트 요청을 처리하는 역할
+
+@RestController
+// 이 클래스가 클라이언트 요청을 처리한다는 것을 표시한다.
+// 이 표시 (애노테이션) 가 붙어 있어야만 스프링부트가 인식한다
 public class ContactController {
 
-  // 인스턴스 변수
-  // => 모든 인스턴스 메서드가 공유한다.
   String[] contacts = new String[5];
   int size = 0;
 
-  @GetMapping("/contact/list") // 클라이언트가 아래 주소로 요청하면 아래 메소드 실행시켜라
+  @RequestMapping("/contact/list") //무조건 앞에 슬래쉬 붙혀야함
   public Object list() {
-    String[] records = new String[size];
-    for (int i=0; i < size; i++) {
-      records[i] = contacts[i];
+    String[] arr = new String[size];
+    for (int i = 0; i < size; i++) {
+      arr[i] = contacts[i]; //  전체 배열에서 값이 들어있는 항목만 복사한다.
     }
-    return records;
+
+    return arr; // 복사한 항목들을 담고 있는 새 배열을 리턴한다.
   }
 
-  @GetMapping("/contact/add") // 클라이언트가 아래 주소로 요청하면 아래 메소드 실행시켜라
+  @RequestMapping("/contact/add") //무조건 앞에 슬래쉬 붙혀야함
   public Object add(String name, String email, String tel, String company) {
-    contacts[size++] = name + "," + email + "," + tel + "," + company;
-
+    String contact = name + "," + email + "," + tel + "," + company;
+    contacts[size++] = contact;
     return size;
   }
 
-  @GetMapping("/contact/get") // 클라이언트가 아래 주소로 요청하면 아래 메소드 실행시켜라
+  @RequestMapping("/contact/get")
   public Object get(String email) {
-    for (int i=0; i < size; i++) {
-      //     if (contact.split(",")[1] == email) { // java는 문자열끼리 비교 못함. js는 가능.
-      if (email.equals(contacts[i].split(",")[1])) {
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) {
         return contacts[i];
       }
     }
-
     return "";
   }
+
+  @RequestMapping("/contact/update")
+  public Object update(String name, String email, String tel, String company) {
+    String contact = name + "," + email + "," + tel + "," + company;
+
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) {
+        contacts[i] = contact;
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  @RequestMapping("/contact/delete")
+  public Object delete(String email) {
+    for (int i = 0; i < size; i++) {
+      if (contacts[i].split(",")[1].equals(email)) { // 예 "ul@test.com"
+
+        // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
+
+        for (int j = i+1; j < size; j++) {
+          contacts[j-1] = contacts[j];
+        }
+      }
+      size--;
+      return 1;
+    }
+    return 0;
+
+
+  }
+
 
 }
---
+++ mylist-boot/app/src/main/java/com/eomcs/mylist/My1Controller.java
@@ -0,0 +1,82 @@
+package com.eomcs.mylist;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class My1Controller {
+
+  String[] contactsPS5 = new String[10];
+  int noPS5 = 0;
+
+  @RequestMapping("/my1/listPS5") //무조건 앞에 슬래쉬 붙혀야함
+  public Object listPS5() {
+    String[] arr = new String[noPS5];
+    for (int i = 0; i < noPS5; i++) {
+      arr[i] = contactsPS5[i]; //  전체 배열에서 값이 들어있는 항목만 복사한다.
+
+
+      //    String[] arr = new String[] {"1, 데몬즈 소울, 액션, 90"};
+    }
+    return arr; // 복사한 항목들을 담고 있는 새 배열을 리턴한다.
+  }
+
+
+
+
+  @RequestMapping("/my1/addPS5") //무조건 앞에 슬래쉬 붙혀야함
+  public Object addPS5(String no, String title, String genre, String critic) {
+    //    no = noPS5 + 1;
+    String contact = (noPS5+1) + "," + title + "," + genre + "," + critic;
+    contactsPS5[noPS5++] = contact;
+    return noPS5;
+  }
+  //
+  @RequestMapping("/my1/getPS5")
+  public Object get(String title) {
+    for (int i = 0; i < noPS5; i++) {
+      if (contactsPS5[i].split(",")[1].equals(title)) {
+        return contactsPS5[i];
+      }
+    }
+    return "";
+  }
+  //
+  @RequestMapping("/my1/updatePS5")
+  public Object update(String no, String title, String genre, String critic) {
+
+    for (int i = 0; i < noPS5; i++) {
+      if (contactsPS5[i].split(",")[1].equals(title)) {
+        String contact = (i+1) + "," +title + "," + genre + "," + critic;
+        contactsPS5[i] = contact;
+        return 1;
+      }
+    }
+
+    return 0;
+  }
+
+  @RequestMapping("/my1/deletePS5")
+  public Object delete(String title, String genre, String critic) {
+    for (int i = 0; i < noPS5; i++) {
+      if (contactsPS5[i].split(",")[1].equals(title)) { // 예 "ul@test.com"
+
+        // 현재 위치의 다음 항목에서 배열 끝까지 반복하며 앞으로 값을 당겨온다.
+
+        for (int j = i+1; j < noPS5; j++) {
+          String contact = (j) + "," +contactsPS5[j].split(",")[1] + "," + contactsPS5[j].split(",")[2] + "," + contactsPS5[j].split(",")[3];
+          contactsPS5[j] = contact;
+          contactsPS5[j-1] = contactsPS5[j];
+        }
+      }
+      noPS5--;
+      return 1;
+    }
+    return 0;
+
+
+  }
+
+
+}
