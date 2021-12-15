ff91686fde8dbb86bbc88104609bc4083e678eb4
InfraWhale <gyqjajang@gmail.com>
2021-12-15 20:34:26 +0900

    20211215

--- java-lang-boot/app/src/main/java/com/eomcs/study/App.java
+++ java-lang-boot/app/src/main/java/com/eomcs/study/App.java
@@ -2,11 +2,13 @@ package com.eomcs.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
+@ComponentScan(basePackages={"com.eomcs.study2","com.eomcs.study"})
 public class App {
 
   public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class App {
 
   @RequestMapping("/hello")
   String hello() {
-    return "Hello World!";
+    return "Hello , java-lang-boot!";
   }
 
 }
