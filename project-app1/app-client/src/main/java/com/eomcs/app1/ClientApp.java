71e0a166a536b939545e6369d3e7485e7f794a84
InfraWhale <gyqjajang@gmail.com>
2022-01-28 18:36:43 +0900

    20220128

--- project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
+++ project-app1/app-client/src/main/java/com/eomcs/app1/ClientApp.java
@@ -8,29 +8,62 @@ public class ClientApp {
 
   public static void main(String[] args) throws Exception {
     System.out.println("[계산기 클라이언트]");
+    Scanner keyScan = new Scanner(System.in);
 
-    // 1) 서버 애플리케이션과 네트워크 연결을 수행한다.
-    Socket socket = new Socket("3.35.54.240", 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
-    System.out.println("서버와 연결되었음");
+    while (true ) {
+      System.out.println("요청(예: 서버주소/연산자/값1/값2)? ");
+      System.out.print("=> ");
+      String input = keyScan.nextLine();
 
-    // 데이터
-    PrintStream out = new PrintStream(socket.getOutputStream());
-    Scanner in = new Scanner(socket.getInputStream());
+      if (input.equals("exit") || input.equals("quit")) {
+        System.out.println("종료");
+        break;
+      }
 
-    // 서버에 데이터를 보낸다.
-    out.println("강사");
+      int slashPos = input.indexOf("/");
+      String serverAddress = input.substring(0, slashPos);
+      String queryString = input.substring(slashPos + 1);
 
-    // 서버가 응답한 데이터를 읽는다.
-    String response = in.nextLine();
-    System.out.println("==>" + response);
 
-    // 입출력 도구 다 사용했으면 자원 해제
-    out.close();
-    in.close();
 
-    // 서버 애플리케이션과 연결된 것을 끊는다.
-    socket.close();
-    System.out.println("서버 연결 종료!");
+      //    if (args.length !=2 ) {
+      //      System.out.println("프로그램 아규먼트가 누락되었습니다.");
+      //      System.out.println("사용법: ClientApp 서버주소 사용자 이름");
+      //      return; // ...........?
+      //    }
+
+      // 1) 서버 애플리케이션과 네트워크 연결을 수행한다.
+      Socket socket = new Socket(serverAddress, 8888); // 서버와 연결될 때까지 객체를 생성하지 않는다.
+      System.out.println("서버와 연결되었음");
+
+      // 데이터를 주고 받기 위한 입출력 스트림을 준비한다.
+      PrintStream out = new PrintStream(socket.getOutputStream());
+      Scanner in = new Scanner(socket.getInputStream());
+
+      // 만약 연산자가 /일경우  %2f문자로 교체한다.
+      if (queryString.startsWith("/")) {
+        queryString = queryString.replaceFirst("/", "%2f"); // 서버에 보내는 데이터 형식에 어긋나지 않도록 인코딩 한다.
+      }
+
+      // 서버에 데이터를 보낸다.
+      out.println(queryString);
+
+      // 서버가 응답한 데이터를 읽는다.
+      String response = in.nextLine();
+      System.out.println("==>" + response);
+
+      // 입출력 도구 다 사용했으면 자원 해제
+      out.close();
+      in.close();
+
+      // 서버 애플리케이션과 연결된 것을 끊는다.
+      socket.close();
+      System.out.println("서버 연결 종료!");
+
+    }
+
+    keyScan.close();
+
   }
 
 }
