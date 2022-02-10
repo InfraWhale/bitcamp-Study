379fc020f364c3291d98dcdcffee3cc870ce54ac
InfraWhale <gyqjajang@gmail.com>
2022-02-10 16:50:16 +0900

    20220210

--- java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/ChatServer.java
@@ -8,71 +8,88 @@ import java.util.ArrayList;
 
 public class ChatServer {
 
-	int port;
+  int port;
 
-	@SuppressWarnings("rawtypes")
-	ArrayList clientOutputStreams = new ArrayList();
+  @SuppressWarnings("rawtypes")
+  ArrayList clientOutputStreams = new ArrayList();
 
-	public ChatServer(int port) {
-		this.port = port;
-	}
+  public ChatServer(int port) {
+    this.port = port;
+  }
 
-	public void service() {
-		try (ServerSocket serverSocket = new ServerSocket(this.port)){
-			System.out.println("서버 실행 중...");
+  public void service() {
+    try (ServerSocket serverSocket = new ServerSocket(this.port)){
+      System.out.println("서버 실행 중...");
 
-			while (true) {
-				new RequestHandler(serverSocket.accept()).start();
-			}
+      while (true) {
+        new RequestHandler(serverSocket.accept()).start();
+      }
 
-		} catch (Exception e) {
-			System.out.println("서버 실행 오류 -" + e.getMessage());
-		}
-	}
+    } catch (Exception e) {
+      System.out.println("서버 실행 오류 -" + e.getMessage());
+    }
+  }
 
-	public void sendMessage(String message) {
-		for (int i = 0; i < clientOutputStreams.size(); i++) {
-			DataOutputStream out = (DataOutputStream) clientOutputStreams.get(i);
-			try {out.writeUTF(message);} catch (Exception e) {}
-		}
-	}
+  @SuppressWarnings({"rawtypes", "unchecked"})
+  public void sendMessage(String message) {
+    ArrayList deleteStreams = new ArrayList();
 
-	class RequestHandler extends Thread {
-		Socket socket;
+    for (int i = 0; i < clientOutputStreams.size(); i++) {
+      DataOutputStream out = (DataOutputStream) clientOutputStreams.get(i);
+      try {
+        out.writeUTF(message);
+      } catch (Exception e) {
+        System.out.println("전송오류:" + message);
+        deleteStreams.add(out); // 무효한 출력 스트림은 삭제 명단에 등록한다.
+      }
+    }
 
-		public RequestHandler(Socket socket) {
-			this.socket = socket;
-		}
+    for (Object deleteStream : deleteStreams) { // 삭제 명단에 등록된 출력 스트림을 제거한다.
+      clientOutputStreams.remove(deleteStream);
+    }
 
-		@SuppressWarnings("unchecked")
-		@Override
-		public void run() {
-			try (Socket socket2 = socket; // 아래 2개 close 자동으로 하려고 하는거임
-					DataOutputStream out = new DataOutputStream(socket.getOutputStream());
-					DataInputStream in = new DataInputStream(socket.getInputStream())) {
+  }
 
-				clientOutputStreams.add(out);
+  class RequestHandler extends Thread {
+    Socket socket;
 
-				out.writeUTF("환영합니다!");
-				out.flush();
+    public RequestHandler(Socket socket) {
+      this.socket = socket;
+    }
 
-				while(true) {
-					String message = in.readUTF();
-					if (message.equals("\\quit")) {
-						out.writeUTF("Goodbye!");
-						break;
-					}
-					sendMessage(message);
-				}
-			} catch (Exception e) {
-				System.out.println("클라이언트와의 통신 오류! - " + e.getMessage());
-			}
-		}
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void run() {
+      try (Socket socket2 = socket; // 아래 2개 close 자동으로 하려고 하는거임
+          DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+          DataInputStream in = new DataInputStream(socket.getInputStream())) {
+
+        clientOutputStreams.add(out);
+
+        String nickname = in.readUTF();
+
+        out.writeUTF(nickname + "님 환영합니다!");
+        out.flush();
+
+        while(true) {
+          String message = in.readUTF();
+          if (message.equals("\\quit")) {
+            out.writeUTF("<![QUIT[]]>"); // 연결을 끊겠다는 특별한 메시지를 클라이언트에게 보낸다.
+            out.flush();
+            clientOutputStreams.remove(out); // 메시지 출력 목록에서 연결이 종료된 클라이언트를 제거한다.//
+            break;
+          }
+          sendMessage(String.format("[%s] %s", nickname, message));
+        }
+      } catch (Exception e) {
+        System.out.println("클라이언트와의 통신 오류! - " + e.getMessage());
+      }
+    }
+  }
 
 
 
-	public static void main(String[] args) {
-		new ChatServer(8888).service();
-	}
+  public static void main(String[] args) {
+    new ChatServer(8888).service();
+  }
 }
--- java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
+++ java-lang/app/src/main/java/com/eomcs/net/ex12/swing/ChatClient.java
@@ -9,7 +9,6 @@ import java.awt.event.WindowEvent;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.net.Socket;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -22,144 +21,180 @@ import javax.swing.UIManager;
 
 
 public class ChatClient extends JFrame{
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	Socket socket;
-	DataInputStream in;
-	DataOutputStream out;
+  Socket socket;
+  DataInputStream in;
+  DataOutputStream out;
+  String nickname ;
 
-	JTextField addressTf = new JTextField(30);
-	JTextField portTf = new JTextField(4);
-	JTextArea messageListTa = new JTextArea();
-	JTextField messageTf = new JTextField(35);
+  JTextField addressTf = new JTextField(30);
+  JTextField portTf = new JTextField(4);
+  JButton connectBtn = new JButton("연결");
 
-	public ChatClient() {
-		super("채팅!!");
-		addWindowListener(new WindowAdapter() { //익명클래스 // 수퍼클래스의 생성자 호출
-			@Override
-			public void windowClosing(WindowEvent e) {
-				try {in.close();} catch (Exception ex) {}
-				try {out.close();} catch (Exception ex) {}
-				try {socket.close();} catch (Exception ex) {}
-				System.exit(0);
-			}
-		});
-		this.setSize(500, 400);
+  JTextArea messageListTa = new JTextArea();
+  JTextField messageTf = new JTextField(35);
 
-		Container contentPane = this.getContentPane();
+  public ChatClient() {
 
-		JPanel topPanel = new JPanel();
-		topPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자 교체
+    String title = "대화명을 입력하세요!\n(2자이상)";
 
-		topPanel.add(addressTf);
-		topPanel.add(portTf);
+    while (true) {
+      nickname = JOptionPane.showInputDialog(title);
+      if (nickname == null) {
+        System.exit(0);
+      } else if (nickname != null && nickname.length() >= 2) {
+        break;
+      }
+      title = "대화명을 다시 입력하세요!\n(2자이상)";
+    }
 
-		JButton connectBtn = new JButton("연결");
+    setTitle("채팅!! -" + nickname);
 
-		// 1) 로컬 클래스
-		//    class MyActionListener implements ActionListener {
-		//      @Override
-		//      public void actionPerformed(ActionEvent e) {
-		//      }
-		//    }
-		//    connectBtn.addActionListener(new MyActionListener());
+    addWindowListener(new WindowAdapter() { //익명클래스 // 수퍼클래스의 생성자 호출
+      @Override
+      public void windowClosing(WindowEvent e) {
+        if(connectBtn.getText().equals("종료")) { //
+          try {
+            out.writeUTF("\\quit");
+            out.flush();
+          } catch (Exception ex) {
+          }
+        }
+        try {in.close();} catch (Exception ex) {}
+        try {out.close();} catch (Exception ex) {}
+        try {socket.close();} catch (Exception ex) {}
+        System.exit(0);
+      }
+    });
+    this.setSize(500, 400);
 
-		// 2) 익명 클래스
-		//    connectBtn.addActionListener(new ActionListener() {
-		//      @Override
-		//      public void actionPerformed(ActionEvent e) {
-		//      }
-		//    });
+    Container contentPane = this.getContentPane();
 
-		// 3) 람다(lambda) 문법
-		//    connectBtn.addActionListener(e -> System.out.println("연결 버튼 눌렀음!"));
+    JPanel topPanel = new JPanel();
+    topPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자 교체
 
-		// 4) 메서드 레퍼런스
-		connectBtn.addActionListener(this::connectChatServer);
-		topPanel.add(connectBtn);
-		contentPane.add(topPanel, BorderLayout.NORTH);
+    topPanel.add(addressTf);
+    topPanel.add(portTf);
 
-		JScrollPane scrollPane = new JScrollPane(messageListTa);
-		contentPane.add(scrollPane, BorderLayout.CENTER);
 
-		JPanel bottomPanel = new JPanel();
-		bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자 교체
-		bottomPanel.add(messageTf);
-		JButton sendBtn = new JButton("보내기");
-		sendBtn.addActionListener(this::sendMessage);
-		bottomPanel.add(sendBtn);
-		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
-		messageTf.addActionListener(this::sendMessage);
+    // 1) 로컬 클래스
+    //    class MyActionListener implements ActionListener {
+    //      @Override
+    //      public void actionPerformed(ActionEvent e) {
+    //      }
+    //    }
+    //    connectBtn.addActionListener(new MyActionListener());
 
-		setVisible(true); // 윈도우 만들어달라고 운영체제에 요청
+    // 2) 익명 클래스
+    //    connectBtn.addActionListener(new ActionListener() {
+    //      @Override
+    //      public void actionPerformed(ActionEvent e) {
+    //      }
+    //    });
 
-	}
-	public static void main(String[] args) throws Exception {
-		UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-		//		UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-		//		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.MetalLookAndFeel");
-		//		UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
-		//		UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"); 없뜸 // 리눅스 OS만 가능
-		//		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");// Windows OS만 가능
-		//		UIManager.setLookAndFeel("com.apple.laf.AquaLookAndFeel");// MAC OS만 가능
-		//		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
-		new ChatClient();
-	}
+    // 3) 람다(lambda) 문법
+    //    connectBtn.addActionListener(e -> System.out.println("연결 버튼 눌렀음!"));
 
-	public void connectChatServer (ActionEvent e) {
-		System.out.println("서버에 연결하기!");
-		System.out.println(addressTf.getText());
-		System.out.println(portTf.getText());
+    // 4) 메서드 레퍼런스
+    connectBtn.addActionListener(this::connectChatServer);
+    topPanel.add(connectBtn);
+    contentPane.add(topPanel, BorderLayout.NORTH);
 
-		try {
-			socket = new Socket(
-					addressTf.getText(), 
-					Integer.parseInt(portTf.getText()));
+    JScrollPane scrollPane = new JScrollPane(messageListTa);
+    contentPane.add(scrollPane, BorderLayout.CENTER);
 
-			in = new DataInputStream(socket.getInputStream());
-			out = new DataOutputStream(socket.getOutputStream());
+    JPanel bottomPanel = new JPanel();
+    bottomPanel.setLayout(new FlowLayout(FlowLayout.LEFT)); // 기본 레이아웃 관리자 교체
+    bottomPanel.add(messageTf);
+    JButton sendBtn = new JButton("보내기");
+    sendBtn.addActionListener(this::sendMessage);
+    bottomPanel.add(sendBtn);
+    contentPane.add(bottomPanel, BorderLayout.SOUTH);
 
-			new MessageReceiver(in).start();
+    messageTf.addActionListener(this::sendMessage);
 
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(this,"서버연결 오류!", "통신 오류", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+    setVisible(true); // 윈도우 만들어달라고 운영체제에 요청
+  }
+  public static void main(String[] args) throws Exception {
+    UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+    //		UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+    //		UIManager.setLookAndFeel("javax.swing.plaf.nimbus.MetalLookAndFeel");
+    //		UIManager.setLookAndFeel("com.sun.java.swing.plaf.motif.MotifLookAndFeel");
+    //		UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel"); 없뜸 // 리눅스 OS만 가능
+    //		UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");// Windows OS만 가능
+    //		UIManager.setLookAndFeel("com.apple.laf.AquaLookAndFeel");// MAC OS만 가능
+    //		UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); 
+    new ChatClient();
+  }
 
-	public void sendMessage(ActionEvent e) {
-		System.out.println("메시지 보내기");
-		if (messageTf.getText().length() == 0) {
-			return;
-		}
+  public void connectChatServer (ActionEvent e) {
+    if (connectBtn.getText().equals("연결")) {
+      try {
+        socket = new Socket(
+            addressTf.getText(), 
+            Integer.parseInt(portTf.getText()));
 
-		try {			
-			out.writeUTF(messageTf.getText());
-			out.flush();
-			messageTf.setText("");
+        in = new DataInputStream(socket.getInputStream());
+        out = new DataOutputStream(socket.getOutputStream());
 
-		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(this,"메시지 전송 오류!", "통신 오류", JOptionPane.ERROR_MESSAGE);
-		}
-	}
+        out.writeUTF(nickname);
+        out.flush();
 
-	class MessageReceiver extends Thread {
+        new MessageReceiver(in).start();
 
-		DataInputStream in;
+      } catch (Exception ex) {
+        JOptionPane.showMessageDialog(this,"서버연결 오류!", "통신 오류", JOptionPane.ERROR_MESSAGE);
+      }
 
-		public MessageReceiver(DataInputStream in) {
-			this.in = in;
-		}
+      connectBtn.setText("종료");
+    } else {
 
-		@Override
-		public void run() {
-			while (true ) {
-				try {
-					String message = in.readUTF();
-					messageListTa.append(message +"\n");
-				} catch (Exception e) {
-				}
-			}
-		}
-	}
+      connectBtn.setText("연결");
+      messageListTa.setText("");
+    }
+  }
+
+  public void sendMessage(ActionEvent e) {
+    System.out.println("메시지 보내기");
+    if (messageTf.getText().length() == 0) {
+      return;
+    }
+
+    try {			
+      out.writeUTF(messageTf.getText());
+      out.flush();
+      messageTf.setText("");
+
+    } catch (Exception ex) {
+      JOptionPane.showMessageDialog(this,"메시지 전송 오류!", "통신 오류", JOptionPane.ERROR_MESSAGE);
+    }
+
+  }
+
+  class MessageReceiver extends Thread {
+
+    DataInputStream in;
+
+    public MessageReceiver(DataInputStream in) {
+      this.in = in;
+    }
+
+    @Override
+    public void run() {
+      while (true ) {
+        try {
+          String message = in.readUTF();
+
+          if (message.equals("<![QUIT[]}>")) { // 서버에서 연결을 끊겠다는 메시지가 오면 스레드를 종료한다.
+            break; // 스레드 종료? run() 메서드의 실행을 마치면 스레드는 종료한다.
+          }
+          messageListTa.append(message +"\n");
+
+        } catch (Exception e) {
+        }
+      }
+    }
+  }
 }
