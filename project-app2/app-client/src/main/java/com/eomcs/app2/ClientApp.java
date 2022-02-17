634999ec2e643967b730cd0f0ed1f6fdc9f6cdc4
InfraWhale <gyqjajang@gmail.com>
2022-02-17 23:05:52 +0900

    .

--- project-app2/app-client/src/main/java/com/eomcs/app2/ClientApp.java
+++ project-app2/app-client/src/main/java/com/eomcs/app2/ClientApp.java
@@ -1,75 +1,68 @@
 package com.eomcs.app2;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-
 import com.eomcs.app2.handler.ScoreHandler;
+import com.eomcs.app2.net.ScoreTableProxy;
 import com.eomcs.util.Prompt;
 
 public class ClientApp {
 
 
-	public static void main(String[] args) {
-		new ClientApp().service();
-	}
+  public static void main(String[] args) {
+    new ClientApp().service();
+  }
 
-	public void service() {
-		try(Socket socket = new Socket("localhost", 3336);
-				ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream()); // 출력스트림 먼저 준비... 왜 ?
-				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());){
+  public void service() {
+    try{
+      ScoreTableProxy scoreTableProxy = new ScoreTableProxy("localhost", 3336);
+      ScoreHandler scoreHandler = new ScoreHandler(scoreTableProxy);
 
-			System.out.println("서버와 연결되었음!");
-			ScoreHandler scoreHandler = new ScoreHandler(in, out);
-
-			while (true) {
-				printMenu();
-				String input = Prompt.promptString("명령");
+      while (true) {
+        printMenu();
+        String input = Prompt.promptString("명령> ");
 
 
-				if (checkQuit(input)){
-					out.writeUTF("quit");
-					out.flush();
-					break;
-				}
+        if (checkQuit(input)){
+          scoreTableProxy.close();
+          break;
+        }
 
-				try{
-					switch (input) {
-					case "1": scoreHandler.create(); break;
-					case "2": scoreHandler.list(); break;
-					case "3": scoreHandler.detail(); break;
-					case "4": scoreHandler.update(); break;
-					case "5": scoreHandler.delete(); break;
-					default:
-						System.out.println("올바른 메뉴 번호를 입력하세요!");
-					}
-				} catch (Exception e) {
-					System.out.println("실행중 오류 발생 : " + e.getMessage());
-				}
+        try{
+          switch (input) {
+            case "1": scoreHandler.create(); break;
+            case "2": scoreHandler.list(); break;
+            case "3": scoreHandler.detail(); break;
+            case "4": scoreHandler.update(); break;
+            case "5": scoreHandler.delete(); break;
+            default:
+              System.out.println("올바른 메뉴 번호를 입력하세요!");
+          }
+        } catch (Exception e) {
+          System.out.println("실행중 오류 발생 : " + e.getMessage());
+        }
 
-				System.out.println();
-			}
-		} catch (Exception e) {
-			System.out.println("서버 통신중 오류 : " + e.getMessage());
-		}
+        System.out.println();
+      }
+    } catch (Exception e) {
+      System.out.println("서버 통신중 오류 : " + e.getMessage());
+    }
 
-		System.out.println("종료!");
-	}
+    System.out.println("종료!");
+  }
 
-	public void printMenu() {
-		System.out.println("메뉴: ");
-		System.out.println("1. 등록");
-		System.out.println("2. 목록");
-		System.out.println("3. 상세");
-		System.out.println("4. 변경");
-		System.out.println("5. 삭제");
-	}
+  public void printMenu() {
+    System.out.println("메뉴: ");
+    System.out.println("1. 등록");
+    System.out.println("2. 목록");
+    System.out.println("3. 상세");
+    System.out.println("4. 변경");
+    System.out.println("5. 삭제");
+  }
 
 
 
-	private boolean checkQuit(String input) {
-		return (input.equals("quit") || input.equals("exit"));
-	}
+  private boolean checkQuit(String input) {
+    return (input.equals("quit") || input.equals("exit"));
+  }
 
 
 }
--- project-app2/app-client/src/main/java/com/eomcs/app2/handler/ScoreHandler.java
+++ project-app2/app-client/src/main/java/com/eomcs/app2/handler/ScoreHandler.java
@@ -1,145 +1,87 @@
 package com.eomcs.app2.handler;
 
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-
+import com.eomcs.app2.net.ScoreTableProxy;
 import com.eomcs.app2.vo.Score;
 import com.eomcs.util.Prompt;
 
 public class ScoreHandler {
 
-	ObjectInputStream in;
-	ObjectOutputStream out;
+  ScoreTableProxy scoreTable;
 
-	public ScoreHandler(ObjectInputStream in, ObjectOutputStream out) {
-		this.in = in;
-		this.out = out;
-	}
+  public ScoreHandler(ScoreTableProxy scoreTable) {
+    this.scoreTable = scoreTable;
+  }
 
-	public void create() throws Exception {
-		Score score = new Score();
-		score.setName(Prompt.promptString("이름? "));
-		score.setKor(Prompt.promptInt("국어? "));
-		score.setEng(Prompt.promptInt("영어? "));
-		score.setMath(Prompt.promptInt("수학? "));
+  public void create() throws Exception {
+    Score score = new Score();
+    score.setName(Prompt.promptString("이름? "));
+    score.setKor(Prompt.promptInt("국어? "));
+    score.setEng(Prompt.promptInt("영어? "));
+    score.setMath(Prompt.promptInt("수학? "));
 
-		out.writeUTF("insert");
-		out.writeObject(score);
-		out.flush();
+    int count = scoreTable.insert(score);
+    if (count == 1) {
+      System.out.println("입력했습니다.");
+    } else {
+      System.out.println("입력하지 못했습니다.");
+    }
+  }
 
-		String status = in.readUTF();
-		if (status.equals("success")) {
-			int count = in.readInt();
-			if (count == 1) {
-				System.out.println("입력했습니다.");
-			} else {
-				System.out.println("입력하지 못했습니다.");
-			}
-		} else {
-			System.out.println(in.readUTF());
-		}
-	}
+  public void list() throws Exception {
+    Score[] scores = scoreTable.selectList();
+    int count = 0;
+    for (Score score : scores) {
+      System.out.printf("%d: %s, %d, %.1f\n",
+          count++,
+          score.getName(), 
+          score.getSum(),
+          score.getAverage());
+    }
+  } 
 
-	public void list() throws Exception {
-		out.writeUTF("selectList");
-		out.flush();
+  public void detail() throws Exception {
+    int no = Prompt.promptInt("번호? ");
 
-		String status = in.readUTF();
-		if (status.equals("success")) {
-			Score[] scores = (Score[]) in.readObject();
-			int count = 0;
-			for (Score score : scores) {
-				System.out.printf("%d: %s, %d, %.1f\n",
-						count++,
-						score.getName(), 
-						score.getSum(),
-						score.getAverage());
-			}
+    Score score = scoreTable.selectOne(no);
 
-		} else {
-			System.out.println(in.readUTF());
-		}
-	}
+    System.out.printf("이름: %s\n", score.getName());
+    System.out.printf("국어: %d\n", score.getKor());
+    System.out.printf("영어: %d\n", score.getEng());
+    System.out.printf("수학: %d\n", score.getMath());
+    System.out.printf("합계: %d\n", score.getSum());
+    System.out.printf("평균: %.1f\n", score.getAverage());
 
-	public void detail() throws Exception {
-		int no = Prompt.promptInt("번호? ");
+  } 
 
-		out.writeUTF("selectOne");
-		out.writeInt(no);
-		out.flush();
 
-		String status = in.readUTF();
-		if (status.equals("success")) {
-			Score score = (Score) in.readObject();
-			System.out.printf("이름: %s\n", score.getName());
-			System.out.printf("국어: %d\n", score.getKor());
-			System.out.printf("영어: %d\n", score.getEng());
-			System.out.printf("수학: %d\n", score.getMath());
-			System.out.printf("합계: %d\n", score.getSum());
-			System.out.printf("평균: %.1f\n", score.getAverage());
 
-		} else {
-			System.out.println(in.readUTF());
-		}
+  public void update() throws Exception {
+    int no = Prompt.promptInt("번호? ");
 
-	}
+    Score old = scoreTable.selectOne(no);
 
-	public void update() throws Exception {
-		int no = Prompt.promptInt("번호? ");
+    Score score = new Score();
+    score.setName(Prompt.promptString("이름(%s)? ", old.getName()));
+    score.setKor(Prompt.promptInt("국어(%d)? ", old.getKor()));
+    score.setEng(Prompt.promptInt("영어(%d)? ", old.getEng()));
+    score.setMath(Prompt.promptInt("수학(%d)? ", old.getMath()));
 
-		out.writeUTF("selectOne");
-		out.writeInt(no);
-		out.flush();
+    int count = scoreTable.update(no,  score);
+    if (count == 1) {
+      System.out.println("변경했습니다.");
+    } else {
+      System.out.println("변경하지 못했습니다.");
+    }
+  } 
 
-		String status = in.readUTF();
-		if (status.equals("success")) {
-			Score old = (Score) in.readObject();
+  public void delete() throws Exception {
+    int no = Prompt.promptInt("번호? ");
 
-			Score score = new Score();
-			score.setName(Prompt.promptString("이름(%s)? ", old.getName()));
-			score.setKor(Prompt.promptInt("국어(%d)? ", old.getKor()));
-			score.setEng(Prompt.promptInt("영어(%d)? ", old.getEng()));
-			score.setMath(Prompt.promptInt("수학(%d)? ", old.getMath()));
-
-			out.writeUTF("update");
-			out.writeInt(no);
-			out.writeObject(score);
-			out.flush();
-
-			status = in.readUTF();
-			if (status.equals("success")) {
-				int count = in.readInt();
-				if (count == 1) {
-					System.out.println("변경했습니다.");
-				} else {
-					System.out.println("변경하지 못했습니다.");
-				}
-			} else {
-				System.out.println(in.readUTF());
-			}
-
-		} else {
-			System.out.println(in.readUTF());
-		}
-	}
-
-	public void delete() throws Exception {
-		int no = Prompt.promptInt("번호? ");
-		out.writeUTF("delete");
-		out.writeInt(no);
-		out.flush();
-
-		String status = in.readUTF();
-		if (status.equals("success")) {
-			int count = in.readInt();
-			if (count == 1) {
-				System.out.println("삭제했습니다.");
-			} else {
-				System.out.println("삭제하지 못했습니다.");
-			}
-		} else {
-			System.out.println(in.readUTF());
-		}
-
-	}
+    int count = scoreTable.delete(no);
+    if (count == 1) {
+      System.out.println("삭제했습니다.");
+    } else {
+      System.out.println("삭제하지 못했습니다.");
+    }
+  }
 }
